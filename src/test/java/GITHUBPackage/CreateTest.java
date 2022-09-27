75997db7d8ec45314a0f15ad7ded909a03157c83
LENOVO <LENOVO@DESKTOP-LT092D8>
2022-09-27 13:04:35 +0530

    Second Commit

--- src/test/java/GITHUBPackage/CreateTest.java
+++ src/test/java/GITHUBPackage/CreateTest.java
@@ -1,5 +1,11 @@
 package GITHUBPackage;
 
-public class CreateTest {
+import org.testng.annotations.Test;
 
+public class CreateTest {
+@Test
+	public void create()
+	{
+	System.out.println("...Created...");
+	}
 }
